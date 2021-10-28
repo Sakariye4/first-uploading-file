6b455aa352f6ed7f0d24ff4ae231693fa2eb3072
sakariye mawliid <sakimowlid@gmail.com>
2021-10-28 08:21:17 -0700

    updated

---
+++ javascrip.js
@@ -0,0 +1,16 @@
+ let age = 75;
+ function lib (msg) {
+     if (age<=15) {
+         console.log('you can not voute 🔐');
+     } 
+     else if (age>=15){
+         console.log('yeah you can vote good lucky  🤣');
+     }
+     else if (age===75) {
+         console.log('now you need to happy go back your home 🤷‍♀️' );
+         
+     }
+     else {
+         console.log('good by see you another time 👍👍')
+     }
+ }
